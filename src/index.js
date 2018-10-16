7e9c2cbc18dc6177f7fda41196540a40ff880e72
hugo <hugo.denisse@skwirrel.fr>
2018-10-16 15:49:33 +0200

    remove wrapping div

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class HttpsRedirect extends React.Component {
-
   isLocalHost(hostname) {
-    return !!(hostname === 'localhost' ||
-              hostname === '[::1]' ||
-              hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));
+    return !!(
+      hostname === 'localhost' ||
+      hostname === '[::1]' ||
+      hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+    );
   }
 
   render() {
@@ -16,19 +17,18 @@ class HttpsRedirect extends React.Component {
       window.location.protocol === 'http:' &&
       !this.isLocalHost(window.location.hostname)
     ) {
-      window.location.href = window.location.href.replace(/^http(?!s)/, 'https');
+      window.location.href = window.location.href.replace(
+        /^http(?!s)/,
+        'https'
+      );
     }
 
-    return (
-      <div>
-        { this.props.children }
-      </div>
-    );
+    return this.props.children;
   }
 }
 
 HttpsRedirect.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 
 export default HttpsRedirect;
