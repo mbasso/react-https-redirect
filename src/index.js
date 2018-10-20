2a734b1db39839c3fdad4cc32017265917cfcd93
mbasso <mbasso96@gmail.com>
2018-10-20 12:14:29 +0200

    general improvement

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 
-class HttpsRedirect extends React.Component {
-  isLocalHost(hostname) {
-    return !!(
-      hostname === 'localhost' ||
-      hostname === '[::1]' ||
-      hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+const isLocalHost = hostname => !!(
+  hostname === 'localhost' ||
+  hostname === '[::1]' ||
+  hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+);
+
+const HttpsRedirect = ({ children }) => {
+  if (
+    typeof window !== 'undefined' &&
+    window.location &&
+    window.location.protocol === 'http:' &&
+    !isLocalHost(window.location.hostname)
+  ) {
+    window.location.href = window.location.href.replace(
+      /^http(?!s)/,
+      'https'
     );
   }
 
-  render() {
-    if (
-      typeof window !== 'undefined' &&
-      window.location &&
-      window.location.protocol === 'http:' &&
-      !this.isLocalHost(window.location.hostname)
-    ) {
-      window.location.href = window.location.href.replace(
-        /^http(?!s)/,
-        'https'
-      );
-    }
-
-    return this.props.children;
-  }
-}
+  return children;
+};
 
 HttpsRedirect.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
 };
 
 export default HttpsRedirect;
